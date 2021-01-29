f9a075d6e65235f441bcb2c821ec1e594993bb51
AlvaroGD99 <alv.gonz.del@gmail.com>
2021-01-29 04:48:31 +0100

    datos inicales creados

---
+++ js/eventos.js
@@ -0,0 +1,21 @@
+const tareas = [
+
+    {
+        id: 1,
+        name: 'Estudiar JS',
+        prioridad: 'importante',
+
+    },
+
+    {
+        id: 2,
+        name: 'Sacar al perro',
+        prioridad: 'diaria',
+    },
+
+    {
+        id: 3,
+        name: 'Salir a comer',
+        prioridad: 'mensual',
+    },
+];
