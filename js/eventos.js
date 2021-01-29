54d5708ba6d8729b56740e170f9071b0be857379
AlvaroGD99 <alv.gonz.del@gmail.com>
2021-01-29 04:54:37 +0100

    añadido evento para incluir mas tareas

--- js/eventos.js
+++ js/eventos.js
@@ -30,3 +30,57 @@ function getTypeNombre(e) {
     }
 
 }
+
+let div = document.querySelector('#alerta')
+let nombreTarea = document.querySelector('#nombreTarea')
+let creacionTarea = document.querySelector('#seleccionPrioridad')
+let guardar = document.querySelector('#guardarTarea')
+guardar.addEventListener('click', guardarTarea)
+function guardarTarea(e) {
+    if (creacionTarea.value == "" || nombreTarea == "") {
+
+        div.style.display = 'block';
+    }
+    else {
+        div.style.display = 'none';
+
+        switch (creacionTarea.value) {
+            case 'importante':
+                idJson++;
+                sectionTareas.innerHTML += `<article style='background-color:beige' data-id='${idJson}'>
+                <h2 style='margin-right:200px; '>${nombreTarea.value.toUpperCase()}</h2>
+                <button id='eliminar'>ELIMINAR</button>
+                </article>`
+
+                let kk = { id: idJson, name: nombreTarea.value, prioridad: creacionTarea.value }
+                tareas.push(kk)
+
+                break;
+
+            case 'diaria':
+                idJson++;
+                sectionTareas.innerHTML += `<article style='background-color:lightblue' data-id='${idJson}'>
+                <h2 style='margin-right:200px;'>${nombreTarea.value.toUpperCase()}</h2>
+                <button id='eliminar'>ELIMINAR</button>
+                </article>`
+
+                let kkk = { id: idJson, name: nombreTarea.value, prioridad: creacionTarea.value }
+                tareas.push(kkk)
+
+                break;
+
+            case 'mensual':
+                idJson++;
+                sectionTareas.innerHTML += `<article style='background-color:lightgreen' data-id='${idJson}'>
+                <h2 style='margin-right:200px;'>${nombreTarea.value.toUpperCase()}</h2>
+                <button id='eliminar'>ELIMINAR</button>
+                </article>`
+
+                let kkkk = { id: idJson, name: nombreTarea.value, prioridad: creacionTarea.value }
+                tareas.push(kkkk)
+
+                break;
+
+        }
+    }
+}
